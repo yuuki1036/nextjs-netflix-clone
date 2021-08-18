f429c4623cc369a42842c47aad01e4de8049925b
yuuki1036 <yuuki1036@gmail.com>
2021-08-18 14:50:39 +0900

    change site title

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
-export const SITE_NAME: string = "NETFJIX";
-export const META_DESCRIPTION: string = "netflix clone site";
+export const SITE_NAME: string = "Netfjix";
+export const META_DESCRIPTION: string = "Netfjix: netflix clone site";
 
 export const API_BASE_URL: string = "https://api.themoviedb.org/3";
 export const IMAGE_BASE_URL: string = "https://image.tmdb.org/t/p/original";
